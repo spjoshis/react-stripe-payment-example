5074de6611003b0f63ba43b9cf3cb53e29cec8b9
Gopal Joshi <gopal.joshi73@hotmail.com>
2018-09-12 17:18:10 +0530

    Updating constants

--- backend-app/constants/frontend.js
+++ backend-app/constants/frontend.js
@@ -1,8 +1,8 @@
 const FRONTEND_DEV_URLS = ['http://localhost:3000'];
 
 const FRONTEND_PROD_URLS = [
-    'https://www.yourdomain.com',
-    'https://yourdomain.com'
+    'https://www.example.com',
+    'https://example.com'
 ];
 
 module.exports = process.env.NODE_ENV === 'production'
